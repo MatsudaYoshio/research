86a9af632f19d27e781a44cd35e451eb5f208744
MatsudaYoshio <dark.unicorn1213@gmail.com>
2018-01-22 16:01:03 +0900

    1€フィルタプログラムの最適化
    スマートポインタに書き換え

--- Source/OneEuroFilter.h
+++ Source/OneEuroFilter.h
@@ -5,20 +5,19 @@
 #define _USE_MATH_DEFINES
 #include "math.h"
 #include <stdexcept>
+#include <memory>
 
 #include "LowPassFilter.h"
 
 class OneEuroFilter {
 private:
 	double freq, mincutoff, beta_, dcutoff;
-	LowPassFilter *x;
-	LowPassFilter *dx;
+	std::unique_ptr<LowPassFilter> x, dx;
 
 	double alpha(const double& cutoff) const;
 public:
 	OneEuroFilter(const double& freq, const double& mincutoff = 1.0, const double& beta_ = 0.0, const double& dcutoff = 1.0);
 	double filter(const double& value);
-	~OneEuroFilter();
 };
 
 #endif
